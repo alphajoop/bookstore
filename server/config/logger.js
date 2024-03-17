666b2d39eba0dcaf14e83deb551762e19e336627
Alpha DIOP <alphakhoss@gmail.com>
2024-03-17 12:17:46 +0000

    feature/logger

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,34 +1,78 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Fonction pour créer le dossier si nécessaire
+const createDirectoryIfNotExists = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
+
+// Créez les dossiers nécessaires
+const logsDirectory = path.join(__dirname, '..', 'logs');
+createDirectoryIfNotExists(logsDirectory);
+
 // Destructure winston's format module
-const { combine, timestamp, printf } = winston.format;
+const { combine, timestamp, printf, colorize } = winston.format;
 
 // Define the log format using a custom printf function
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
+// Define the format for console output including enriched information
+const consoleFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  let logInfo = `${timestamp} ${level}: ${message}`;
+  if (Object.keys(metadata).length > 0) {
+    logInfo += `\n${JSON.stringify(metadata, null, 2)}`;
+  }
+  return logInfo;
+});
+
 // Create a new logger instance with specified configuration
 const logger = winston.createLogger({
-  level: 'info', // Minimum logging level
+  level: 'info',
   format: combine(
-    timestamp(), // Add a timestamp to each log entry
-    logFormat // Use the defined log format
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    logFormat
   ),
   transports: [
-    // Transport to write logs to a file
-    new winston.transports.File({
-      filename: path.join(__dirname, '..', 'logs', 'app.log'),
-      maxsize: 5242880, // Maximum size of the log file (5MB)
-      maxFiles: 5, // Maximum number of log files (rotation)
+    // Transport to write logs to a daily rotating file
+    new DailyRotateFile({
+      filename: path.join(logsDirectory, 'app-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD', // Date format in file name
+      zippedArchive: true, // Archive previous log files
+      maxSize: '5m', // Maximum size of a log file
+      maxFiles: '5d', // Maximum number of log files to keep
     }),
     // Transport to log messages to the console (useful during development)
-    new winston.transports.Console(),
+    new winston.transports.Console({
+      format: combine(
+        colorize(),
+        consoleFormat
+      ),
+      level: process.env.NODE_ENV === 'prod' ? 'info' : 'debug',
+    }),
+    // Transport to handle errors
+    new winston.transports.File({
+      filename: path.join(logsDirectory, 'error.log'),
+      level: 'error',
+    }),
+  ],
+  exceptionHandlers: [
+    // Transport to handle uncaught exceptions
+    new winston.transports.File({
+      filename: path.join(logsDirectory, 'exceptions.log'),
+    }),
   ],
 });
 
--- server/middlewares/loggerMiddleware.js
+++ server/middlewares/loggerMiddleware.js
@@ -1,8 +1,34 @@
+// Supprimez l'importation de chalk dans votre fichier de configuration de journal
 import logger from '../config/logger.js';
 
 const loggerMiddleware = (req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
-  next();
+  const start = Date.now();
+
+  let requestType = '';
+  if (req.headers['user-agent'] && req.headers['user-agent'].includes('Mozilla')) {
+    requestType = 'WEB';
+  } else if (req.headers['user-agent'] && req.headers['user-agent'].includes('PostmanRuntime')) {
+    requestType = 'API';
+  } else {
+    requestType = 'OTHER';
+  }
+
+  logger.info(`${requestType} ${req.method} ${req.url} ${req.ip} Content-Type: ${JSON.stringify(req.headers['content-type'] || 'Not specified')}`);
+
+  // Gestion des erreurs dans le middleware
+  try {
+    // Exécuter la requête
+    next();
+  } catch (error) {
+    // Capturer et journaliser les erreurs
+    logger.error(`Error occurred: ${error.message}`);
+    // Passer l'erreur au gestionnaire d'erreurs global
+    next(error);
+  } finally {
+    const end = Date.now();
+    const duration = end - start; // Calculer la durée de la requête en millisecondes
+    logger.info(`Request duration: ${duration}ms`);
+  }
 };
 
 export default loggerMiddleware;
